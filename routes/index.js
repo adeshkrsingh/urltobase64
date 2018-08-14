3ae9703bdc4aa80524f95c91c8a04da40b7c9951
megastar <megastar@localhost.localdomain>
2018-08-14 17:00:25 +0530

    updated

--- routes/index.js
+++ routes/index.js
@@ -403,31 +403,53 @@ router.post("/", function(req, res, next) {
     console.log("RUNNING POST METHOD");
     const $ = cheerio.load( req.body.content );
     // console.log(req.body.content );
-  var list = [], options = []; let correctOpt = 0; let OptionNumber = 0;
-  $('div[id="question_basic"]')
+  var list = [], options = []; let correctOpt = 0; let OptionNumber = 0; let explanation = "";
+
+  $('.question_basic')
     .find("div > .question_text ")
     .each(function(index, element) {
-      list.push($(element).text());
+      list.push($(element).html());
 
       console.log( $(element).html().trim() );
       $(this).parent().find('div > table > tbody > tr >  .option_text').each(function(i, elemt) {
         OptionNumber++;
+
         console.log(OptionNumber, $(elemt).text().trim() );
-        $(this).parent().find('.q_tbl_optn_col_1 > i').each(function(i3, e3) {
+        let ch =  $(this).parent().find('.q_tbl_optn_col_1 > i').each(function(i3, e3) {
             console.log( 'Correct Opt : ',OptionNumber,  $(e3).attr('data-original-title').trim() );
         });
+
+        if(ch == '') {
+            console.log('*****');
+            options.push({
+                opt: $(elemt).text().trim(),
+                is_correct: 0,
+            });
+        } else {
+            console.log('######');
+            options.push({
+                opt: $(elemt).text().trim(),
+                is_correct: 1,
+            });
+        }
     });
 
     if( $(this).parent().find('.explanation_text').children().first().text().trim().length > 0 ) {
-        console.log( $(this).parent().find('.explanation_text').children().first().html().trim() );
+        explanation = $(this).parent().find('.explanation_text').children().first().html().trim();
+        console.log( explanation );
     } else {
-        console.log( $(this).parent().find('.explanation_text').html().trim() );
+        explanation = $(this).parent().find('.explanation_text').html().trim();
+        console.log( explanation );
     }
 
     // console.log( $(this).parent().find('.explanation_text').children().first().text() );
     OptionNumber = 0;
     });
-  res.render("index", { title: "Express" });
+  res.render("add_question", { title: "Express",
+                                question: list[0],
+                                options: options,
+                                explanation: explanation,
+                                 });
 });
 
 module.exports = router;
